4b0e2c58f7006838750fbc63a9fa62a5b7a1da5c
Jose Angel Rodriguez <jskcod4@gmail.com>
2024-10-08 00:18:35 -0400

    Add lambda for aws products

---
+++ ms-aws-store-products/src/handler.ts
@@ -0,0 +1,32 @@
+'use strict'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb')
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0.qtzr3.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri)
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    await client.connect()
+
+    const db = client.db(process.env.MONG_DB_NAME)
+    const collection = db.collection('products')
+    const products = await collection.find({}).toArray()
+
+    return {
+      statusCode: 200,
+      body: products
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  } finally {
+    await client.close()
+  }
+}
