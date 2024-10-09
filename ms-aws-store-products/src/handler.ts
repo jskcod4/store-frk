b016bef8b0db3dc59bd8742759303842f9a1aabc
Jose Angel Rodriguez <jskcod4@gmail.com>
2024-10-09 00:19:18 -0400

    Change order execution

--- ms-aws-store-products/src/handler.ts
+++ ms-aws-store-products/src/handler.ts
@@ -8,18 +8,6 @@ const { MongoClient } = require('mongodb')
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0.qtzr3.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri)
 
-console.log('process.env.MONGO_USERNAME', process.env.MONGO_USERNAME)
-
-if (!process.env.MONGO_USERNAME) {
-  console.log('process.env.MONGO_USERNAME is not defined')
-}
-
-console.log('process.env.MONGO_PW', process.env.MONGO_PW)
-
-if (!process.env.MONGO_PW) {
-  console.log('process.env.MONGO_PW is not defined')
-}
-
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     await client.connect()
